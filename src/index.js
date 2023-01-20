5b8c7e837ab3c29bae41fe5b1dc695ab32e5a995
guilhermehub12 <guilhermee1312@hotmail.com>
2023-01-19 22:20:02 -0300

    build: code refactoring

--- src/index.js
+++ src/index.js
@@ -2,18 +2,22 @@ const productImage = document.querySelector("#sofa");
 const changeIcon = document.querySelector("#icon");
 const sofa3D = document.querySelector("#sofa-3d");
 
-changeIcon.addEventListener("click", toggleImageToGif);
+changeIcon.addEventListener("click", toggleImage);
 
-function toggleImageToGif() {
-    if (changeIcon.classList.contains("active")){
-        productImage.style.display = "none"
-        sofa3D.innerHTML = `<model-viewer src="src/assets/scene.gltf" alt="Sofa" shadow-intensity="1" camera-controls ar touch-action="pan-y">`
-        changeIcon.src = "src/imgs/icon-close.svg"
-        changeIcon.classList.remove('active')
-    }else {
-        productImage.style.display = "block";
-        sofa3D.innerHTML = ""
-        changeIcon.src = "src/imgs/icon-360g.svg"
-        changeIcon.classList.add("active")
-    }
+function toggleImage() {
+    changeIcon.classList.contains("active") ? show3DModel() : showProductImage();
+}
+
+function show3DModel() {
+    productImage.style.display = "none";
+    sofa3D.innerHTML = `<model-viewer src="src/assets/scene.gltf" alt="Sofa" shadow-intensity="1" camera-controls ar touch-action="pan-y">`;
+    changeIcon.src = "src/imgs/icon-close.svg";
+    changeIcon.classList.remove('active');
+}
+
+function showProductImage() {
+    productImage.style.display = "block";
+    sofa3D.innerHTML = "";
+    changeIcon.src = "src/imgs/icon-360g.svg";
+    changeIcon.classList.add("active");
 }
