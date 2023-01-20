76bc509401926919366b89a9a5ee9870610d2bb6
guilhermehub12 <guilhermee1312@hotmail.com>
2023-01-19 21:56:28 -0300

    feat: change to a 3d model sofa using model viewer

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 const productImage = document.querySelector("#sofa");
 const changeIcon = document.querySelector("#icon");
+const sofa3D = document.querySelector("#sofa-3d");
 
 changeIcon.addEventListener("click", toggleImageToGif);
 
 function toggleImageToGif() {
     if (changeIcon.classList.contains("active")){
-        productImage.src = "src/imgs/sofa-animated.gif";
+        productImage.style.display = "none"
+        sofa3D.innerHTML = `<model-viewer src="src/assets/scene.gltf" alt="Sofa" shadow-intensity="1" camera-controls ar touch-action="pan-y">`
         changeIcon.src = "src/imgs/icon-close.svg"
         changeIcon.classList.remove('active')
     }else {
-        productImage.src = "src/imgs/sofa.svg";
+        productImage.style.display = "block";
+        sofa3D.innerHTML = ""
         changeIcon.src = "src/imgs/icon-360g.svg"
         changeIcon.classList.add("active")
     }
